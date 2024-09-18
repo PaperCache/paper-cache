ba7a1339a6686d8e8fe1e74e303a07595eba289c
Kia Shakiba <kiashakiba@protonmail.com>
2024-09-18 03:47:04 -0400

    Update error messages + upgrade to v1.3.6

--- src/error.rs
+++ src/error.rs
@@ -2,27 +2,27 @@ use thiserror::Error;
 
 #[derive(Debug, PartialEq, Error)]
 pub enum CacheError {
-	#[error("At least one policy must be configured.")]
+	#[error("internal error")]
+	Internal,
+
+	#[error("at least one policy must be configured")]
 	EmptyPolicies,
 
-	#[error("Duplicate policies were configured.")]
+	#[error("duplicate policies were configured")]
 	DuplicatePolicies,
 
-	#[error("The supplied policy is not one of the cache's configured policies.")]
+	#[error("the supplied policy is not one of the cache's configured policies")]
 	UnconfiguredPolicy,
 
-	#[error("The key was not found in the cache.")]
+	#[error("the key was not found in the cache")]
 	KeyNotFound,
 
-	#[error("The value size cannot be zero.")]
+	#[error("the value size cannot be zero")]
 	ZeroValueSize,
 
-	#[error("The value size cannot exceed the cache size.")]
+	#[error("the value size cannot exceed the cache size")]
 	ExceedingValueSize,
 
-	#[error("The cache size cannot be zero.")]
+	#[error("the cache size cannot be zero")]
 	ZeroCacheSize,
-
-	#[error("Internal error.")]
-	Internal,
 }
