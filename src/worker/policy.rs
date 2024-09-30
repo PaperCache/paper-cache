5ff2c8c5aaa7881536fc96e93782c4918971ad94
Kia Shakiba <kiashakiba@protonmail.com>
2024-09-30 16:20:03 -0400

    Refactor policy worker listener

--- src/worker/policy.rs
+++ src/worker/policy.rs
@@ -12,7 +12,7 @@ use std::{
 };
 
 use typesize::TypeSize;
-use crossbeam_channel::{Receiver, unbounded};
+use crossbeam_channel::{Sender, Receiver, unbounded};
 
 use kwik::{
 	time,
@@ -91,14 +91,8 @@ where
 					WorkerEvent::Get(key, hit) if hit => self.handle_get(key),
 
 					WorkerEvent::Set(key, size, _, old_size) => {
-						self.handle_set(key);
+						self.handle_set(key, size, old_size);
 						has_current_set = true;
-
-						self.used_cache_size += size;
-
-						if let Some(old_size) = old_size {
-							self.used_cache_size -= old_size;
-						}
 					},
 
 					WorkerEvent::Del(key, size, _) => {
@@ -114,35 +108,7 @@ where
 					WorkerEvent::Resize(max_cache_size) => self.max_cache_size = max_cache_size,
 
 					WorkerEvent::Policy(policy) => {
-						let is_current_policy = self.policy_stack
-							.as_ref()
-							.is_some_and(|policy_stack| policy_stack.is_policy(policy));
-
-						if !is_current_policy {
-							let mini_policy_index = self.mini_policy_stacks
-								.iter()
-								.position(|mini_stack| mini_stack.is_policy(policy))
-								.unwrap_or(0);
-
-							self.policy_stack = None;
-							self.mini_policy_index = Some(mini_policy_index);
-
-							let max_cache_size = self.max_cache_size;
-							let traces = self.traces.clone();
-							let policy_reconstruct_tx = policy_reconstruct_tx.clone();
-
-							thread::spawn(move || {
-								let reconstruction_result = reconstruct_policy_stack::<K>(
-									policy,
-									max_cache_size,
-									traces.clone(),
-								);
-
-								if let Ok(stack) = reconstruction_result {
-									let _ = policy_reconstruct_tx.send(stack);
-								}
-							});
-						}
+						self.handle_policy(policy, policy_reconstruct_tx.clone());
 					},
 
 					_ => {},
@@ -153,67 +119,8 @@ where
 				}
 			}
 
-			for mut reconstructed_stack in policy_reconstruct_rx.try_iter() {
-				for event in &buffered_events {
-					match event {
-						WorkerEvent::Get(key, hit) if *hit => reconstructed_stack.update(*key),
-						WorkerEvent::Set(key, _, _, _) => reconstructed_stack.insert(*key),
-						WorkerEvent::Del(key, _, _) => reconstructed_stack.remove(*key),
-						WorkerEvent::Wipe => reconstructed_stack.clear(),
-
-						_ => {},
-					}
-				}
-
-				buffered_events.clear();
-
-				self.policy_stack = Some(reconstructed_stack);
-				self.mini_policy_index = None;
-			}
-
-			match (self.policy_stack.as_mut(), self.mini_policy_index) {
-				(Some(policy_stack), None) => {
-					while self.used_cache_size > self.max_cache_size {
-						if let Some(key) = policy_stack.pop() {
-							if let Ok(object) = erase(&self.objects, &self.stats, &self.overhead_manager, key) {
-								self.used_cache_size -= self.overhead_manager.total_size(key, &object);
-							}
-						}
-					}
-				},
-
-				(None, Some(mini_policy_index)) => {
-					let mini_policy_stack = &mut self.mini_policy_stacks[mini_policy_index];
-					let mut evictions = Vec::<K>::new();
-
-					while self.used_cache_size > self.max_cache_size {
-						if let Some(key) = mini_policy_stack.pop() {
-							if let Ok(object) = erase(&self.objects, &self.stats, &self.overhead_manager, key) {
-								let size = self.overhead_manager.total_size(key, &object);
-								self.used_cache_size -= size;
-
-								evictions.push(key);
-								buffered_events.push(WorkerEvent::Del(key, size, None));
-							}
-						} else {
-							// the mini stack is empty, but it's okay because the cache will just use
-							// a little more memory until the policy stack is reconstructed, so we want
-							// to be sure we don't get stuck here
-							break;
-						}
-					}
-
-					for key in &evictions {
-						for (index, mini_stack) in self.mini_policy_stacks.iter_mut().enumerate() {
-							if index != mini_policy_index {
-								mini_stack.remove(*key);
-							}
-						}
-					}
-				},
-
-				_ => unreachable!(),
-			}
+			self.apply_buffered_events(&mut buffered_events, &policy_reconstruct_rx);
+			self.apply_evictions(&mut buffered_events);
 
 			let now = time::timestamp();
 
@@ -287,13 +194,19 @@ where
 		}
 	}
 
-	fn handle_set(&mut self, key: K) {
+	fn handle_set(&mut self, key: K, size: ObjectSize, old_size: Option<ObjectSize>) {
 		if let Some(stack) = &mut self.policy_stack {
 			stack.insert(key);
 		}
 
 		for mini_stack in &mut self.mini_policy_stacks {
 			mini_stack.insert(key);
+		}
+
+		self.used_cache_size += size;
+
+		if let Some(old_size) = old_size {
+			self.used_cache_size -= old_size;
 		}
 	}
 
@@ -307,6 +220,43 @@ where
 		}
 	}
 
+	fn handle_policy(
+		&mut self,
+		policy: PaperPolicy,
+		policy_reconstruct_tx: Arc<Sender<PolicyStackType<K>>>,
+	) {
+		let is_current_policy = self.policy_stack
+			.as_ref()
+			.is_some_and(|policy_stack| policy_stack.is_policy(policy));
+
+		if is_current_policy {
+			return;
+		}
+
+		let mini_policy_index = self.mini_policy_stacks
+			.iter()
+			.position(|mini_stack| mini_stack.is_policy(policy))
+			.unwrap_or(0);
+
+		self.policy_stack = None;
+		self.mini_policy_index = Some(mini_policy_index);
+
+		let max_cache_size = self.max_cache_size;
+		let traces = self.traces.clone();
+
+		thread::spawn(move || {
+			let reconstruction_result = reconstruct_policy_stack::<K>(
+				policy,
+				max_cache_size,
+				traces.clone(),
+			);
+
+			if let Ok(stack) = reconstruction_result {
+				let _ = policy_reconstruct_tx.send(stack);
+			}
+		});
+	}
+
 	fn handle_wipe(&mut self) {
 		if let Some(stack) = &mut self.policy_stack {
 			stack.clear();
@@ -314,6 +264,87 @@ where
 
 		for mini_stack in &mut self.mini_policy_stacks {
 			mini_stack.clear();
+		}
+	}
+
+	fn apply_buffered_events(
+		&mut self,
+		buffered_events: &mut Vec<WorkerEvent<K>>,
+		policy_reconstruct_rx: &Receiver<PolicyStackType<K>>,
+	) {
+		for mut reconstructed_stack in policy_reconstruct_rx.try_iter() {
+			for event in buffered_events.iter() {
+				match event {
+					WorkerEvent::Get(key, hit) if *hit => reconstructed_stack.update(*key),
+					WorkerEvent::Set(key, _, _, _) => reconstructed_stack.insert(*key),
+					WorkerEvent::Del(key, _, _) => reconstructed_stack.remove(*key),
+					WorkerEvent::Wipe => reconstructed_stack.clear(),
+
+					_ => {},
+				}
+			}
+
+			buffered_events.clear();
+
+			self.policy_stack = Some(reconstructed_stack);
+			self.mini_policy_index = None;
+		}
+	}
+
+	fn apply_evictions(&mut self, buffered_events: &mut Vec<WorkerEvent<K>>) {
+		if let Some(index) = self.mini_policy_index {
+			self.apply_mini_evictions(index, buffered_events);
+		}
+
+		if let Some(stack) = self.policy_stack.as_mut() {
+			while self.used_cache_size > self.max_cache_size {
+				let Some(key) = stack.pop() else {
+					continue;
+				};
+
+				let Ok(object) = erase(&self.objects, &self.stats, &self.overhead_manager, key) else {
+					continue;
+				};
+
+				self.used_cache_size -= self.overhead_manager.total_size(key, &object);
+			}
+		}
+	}
+
+	fn apply_mini_evictions(
+		&mut self,
+		mini_policy_index: usize,
+		buffered_events: &mut Vec<WorkerEvent<K>>,
+	) {
+		let mini_policy_stack = &mut self.mini_policy_stacks[mini_policy_index];
+		let mut evictions = Vec::<K>::new();
+
+		while self.used_cache_size > self.max_cache_size {
+			let Some(key) = mini_policy_stack.pop() else {
+				// the mini stack is empty, but it's okay because the cache will just use
+				// a little more memory until the policy stack is reconstructed, so we want
+				// to be sure we don't get stuck here
+
+				break;
+			};
+
+			let Ok(object) = erase(&self.objects, &self.stats, &self.overhead_manager, key) else {
+				continue;
+			};
+
+			let size = self.overhead_manager.total_size(key, &object);
+			self.used_cache_size -= size;
+
+			evictions.push(key);
+			buffered_events.push(WorkerEvent::Del(key, size, None));
+		}
+
+		for key in &evictions {
+			for (index, mini_stack) in self.mini_policy_stacks.iter_mut().enumerate() {
+				if index != mini_policy_index {
+					mini_stack.remove(*key);
+				}
+			}
 		}
 	}
 }
